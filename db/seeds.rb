3b0f952d4ff403f5efa4192986d386125dc47ca8
Brian Sung <bksung0708@gmail.com>
2017-09-25 13:21:59 -0700

    pedia 6 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+    User.create!(
+                email: Faker::Internet.email,
+                password: Faker::Config.random.seed
+                )
+end
+users = User.all
+
+10.times do
+    Wiki.create!(
+                user: users.sample,
+                title: Faker::Company.bs,
+                body: Faker::Company.bs,
+                private: false
+                )
+end
+wikis = Wiki.all
+
+puts "Seed finished."
+puts "#{User.count} users created."
+puts "#{Wiki.count} wikis created."
