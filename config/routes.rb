fde2ff3b6ae1a58231721e8d5944428dfe8bf00a
Brian Sung <bksung0708@gmail.com>
2017-10-02 20:37:56 -0700

    pedia 10_3 in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :wikis
+  resources :wikis do
+    resources :collaborators, only: [:edit, :destroy]
+  end
 
   resources :charges, only: [:new, :create, :edit]
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927094430) do
+ActiveRecord::Schema.define(version: 20171003032908) do
+
+  create_table "collaborators", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "wiki_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_collaborators_on_user_id"
+    t.index ["wiki_id"], name: "index_collaborators_on_wiki_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,7 +46,6 @@ ActiveRecord::Schema.define(version: 20170927094430) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "markdown_content"
     t.index ["user_id"], name: "index_wikis_on_user_id"
   end
 
